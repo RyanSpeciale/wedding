758c8b8c07bfdaa93bf7941993af51b87e6247e1
ryanspeciale <difficultoctet@gmail.com>
2022-03-17 15:51:14 -0400

    started the details page

--- pages/weddingdetails.tsx
+++ pages/weddingdetails.tsx
@@ -1,9 +1,22 @@
-
+import styles from '../styles/details.module.css'
+import { Tabs, Tab } from 'grommet';
 
 const weddingdetails = () => {
+   
+   
     return (
         <div className={styles.container}>
-                        
+            <Tabs margin='small' className={styles.tabcontainer}>
+                <Tab title='The Ceremony'>
+
+                </Tab>
+                <Tab title='The Reception'>
+
+                </Tab>
+                <Tab title='Accomidations and Other Info'>
+
+                </Tab>
+            </Tabs>                
         </div>
     );
 };
