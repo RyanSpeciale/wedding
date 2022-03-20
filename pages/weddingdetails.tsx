c8118193244a001ac23bd85d69986f806181a1ac
ryanspeciale <difficultoctet@gmail.com>
2022-03-20 12:47:32 -0400

    finished the design of the first tab

--- pages/weddingdetails.tsx
+++ pages/weddingdetails.tsx
@@ -1,26 +1,37 @@
 import styles from '../styles/details.module.css'
-import { Tabs, Tab } from 'grommet';
+import { Tabs, Tab, Text } from 'grommet';
 import { Box } from 'grommet';
+import Image from 'next/image';
+import church from '../photos/church.jpg';
 
 const weddingdetails = () => {
    
    
     return (
         <div className={styles.container}>
-            <Tabs margin='medium' color='black' className={styles.tabcontainer}>
-                <Tab title='The Ceremony' color='black'>
-                    <Box>
+            <Tabs margin='medium' justify='center' className={styles.tabcontainer}>
+                <Tab title='The Ceremony' color='black' >
+                    <Box justify='center' elevation='small' round='medium' direction='row' pad='1.5cm' border={{}} className={styles.ceremony}>
+                        <Image alt='photo of church' src={church} />
+                        <Text className={styles.ceremonytext} size='medium'>
+                            The Ceremony will be held at Westmont Presbyterian Church in Johnstown, PA.
+                            This church was Leah's growing up as a child, and it means so much for her to get married in it.
 
+                        </Text>
                     </Box>
                 </Tab>
                 <Tab title='The Reception' color='black'>
-                    <Box>
+                    <Box pad='medium'>
+                        <Text>
 
+                        </Text>
                     </Box>
                 </Tab>
                 <Tab title='Accomidations and Other Info' color='black'>
-                    <Box>
+                    <Box pad='large'>
+                        <Text>
 
+                        </Text>
                     </Box>
                 </Tab>
             </Tabs>                
