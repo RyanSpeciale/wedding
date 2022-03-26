499aa7a978a276547447edfb03def2f0a6e322c2
ryanspeciale <difficultoctet@gmail.com>
2022-03-26 09:50:15 -0400

    decided on hotels, finishing hotel cards

---
+++ components/HotelCard.tsx
@@ -0,0 +1,16 @@
+import Card from 'grommet';
+import CardHeader from 'grommet';
+imp 
+
+interface props {
+    title: string,
+
+}
+
+const HotelCard: React.FC<props> = () => {
+    return (
+        
+    );
+};
+
+export default HotelCard;
---
+++ pages/photos.tsx
@@ -0,0 +1,11 @@
+
+
+const photos = () => {
+    return (
+        <div>
+            <h1>Wedding Photos</h1>
+        </div>
+    );
+};
+
+export default photos;
