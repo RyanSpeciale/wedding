71a49346f68a4ebf83f97baa4125752db74f01c0
ryanspeciale <difficultoctet@gmail.com>
2022-03-30 20:50:20 -0400

    frontend views finished, frontend logic next

--- pages/weddingdetails.tsx
+++ pages/weddingdetails.tsx
@@ -4,7 +4,7 @@ import { Box } from 'grommet';
 import Image from 'next/image';
 import church from '../photos/church.jpg';
 import greengables from '../photos/greengables.jpg';
-import quality from '../photos/qualityinn.jpg';
+import holidy from '../photos/holiday-inn-johnstown.jpg';
 import coalminer from '../photos/coalminer.jpg';
 
 const weddingdetails: React.FC = () => {
@@ -58,14 +58,14 @@ const weddingdetails: React.FC = () => {
                             <Text className={styles.title} size='large' textAlign='center'>There are plenty of places to stay near Green Gables, we have a couple listed here for your convenience.</Text>
                             <Box direction='row-responsive' gap='medium' justify='center'margin='small' responsive={true} alignContent='center' align='center' wrap={true}>
                                 <Card height='medium' width='medium' background='light-4' className={styles.card}>
-                                    <CardHeader pad='xsmall' margin={{ bottom: '0.5cm'}} className={styles.cardtext}>Quality Inn Somerset</CardHeader>
+                                    <CardHeader pad='xsmall' margin={{ bottom: '0.5cm'}} className={styles.cardtext}>Holiday Inn Johnstown-Downtown</CardHeader>
                                     <Box direction='row' align='center' alignSelf='center' alignContent='content' basis='1/2'>
-                                        <Image alt='photo of hotel' src={quality} height={200} width={250} />
+                                        <Image alt='photo of hotel' src={holidy} height={200} width={300} />
                                     </Box>
                                     <Box direction='row' align='center' alignContent='center' alignSelf='center' margin={{ top: '1cm' }}>
-                                        <Text textAlign='center'>215 Ramada Rd, Somerset, PA 15501, 12 miles from Green Gables, 10% discount, Close to the Turkpike.</Text>
+                                        <Text textAlign='center'>Host Hotel, this is where we suggest you stay. 250 Market St, Johnstown, PA 15901. We are looking into a shuttle to and from Green Gables.</Text>
                                     </Box>
-                                    <Box gap='medium' direction='row' align='center' alignContent='center' justify='center' margin={{ top: '0.4cm', bottom: '0.2cm'}}>
+                                    <Box gap='medium' direction='row' align='center' alignContent='center' justify='center' margin={{ top: '0.5cm', bottom: '0.2cm'}}>
                                         <Button primary color='black' label='Website' />
                                         <Button primary color='black' label='Booking'/>
                                     </Box>
@@ -73,10 +73,10 @@ const weddingdetails: React.FC = () => {
                                 <Card height='medium' width='medium' background='light-4' className={styles.card}>
                                     <CardHeader pad='xsmall' margin={{ bottom: '0.5cm'}} className={styles.cardtext}>Coal Miner's Diner</CardHeader>
                                     <Box direction='row' align='center' alignSelf='center' alignContent='content' basis='1/2'>
-                                        <Image alt='photo of hotel' src={coalminer} height={200} width={250} />
+                                        <Image alt='photo of hotel' src={coalminer} height={200} width={300} />
                                     </Box>
                                     <Box direction='row' align='center' alignContent='center' alignSelf='center' margin={{ top: '1cm' }}>
-                                        <Text textAlign='center'>1640 US-30, Jennerstown, PA 15547. Not even a 2 minute drive from Green Gables, Breakfast Included.</Text>
+                                        <Text textAlign='center'>If the Holiday Inn doesn't work for you. About a 2 minute drive from Green Gables. 1640 US-30, Jennerstown, PA 15547.</Text>
                                     </Box>
                                     <Box gap='medium' direction='row' align='center' alignContent='center' justify='center' margin={{ top: '0.4cm', bottom: '0.2cm'}}>
                                         <Button primary color='black' label='Website' />
