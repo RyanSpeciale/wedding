29ea00d03da0430cecb46cd70a7c226601effe85
ryanspeciale <difficultoctet@gmail.com>
2022-04-06 19:27:56 -0400

    last chance

--- pages/api/rsvp.js
+++ pages/api/rsvp.js
@@ -1,20 +1,24 @@
 import prisma from '../../lib/prisma';
 
 export default async function handler(req, res) {
-    const { first, last, email, attending, meal, kids } = req.body;
-    try {
-        const result = await prisma.guest.create({
-            data: {
-                first: first,
-                last: last,
-                email: email,
-                attending: attending,
-                meal: meal,
-                kids: kids
-            }
-        });
-        res.json({ success: result });
-    } catch(error) {
-        res.status(402).json({ error: error })
+    const { first, last, attending, email, meal, kids } = req.body;
+    if (req.method == 'POST') {
+        try {
+            const result = await prisma.guest.create({
+                data: {
+                    first: first,
+                    last: last,
+                    attending: attending,
+                    email: email,
+                    meal: meal,
+                    kids: kids
+                }
+            })
+            res.status(202).json({ success: result})
+        } catch(error) {
+            res.status(403).json({ dberror: error})
+        }
+    } else {
+        res.status(404)
     }
 }
