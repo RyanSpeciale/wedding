79ca3476d05325f62e33dc37e3d7f0c064588936
ryanspeciale <difficultoctet@gmail.com>
2022-04-08 19:19:07 -0400

    final destination 4

--- pages/rsvp.tsx
+++ pages/rsvp.tsx
@@ -14,10 +14,10 @@ const RSVP = () => {
         <div className={styles.container}>
             <Box>
                 <Box justify='center' align='center' pad='small' border={{ color: 'black', style: 'dotted', size: 'medium' }} round='medium' alignContent='center' gap='medium' margin='medium' className={styles.box}>
-                    <Text textAlign='center' alignSelf='center' weight='bold'>
-                      Please click the button below to RSVP. This button will take you to a Google Form to collect your information.
-                      If you want to continue to browse the website while you fill out the form, you can always right click on the button and click &quot;Open Link In New Tab&quot;.
-                      Once you submit the form, you are good to go! You will recieve an email with your RSVP responses and if you ever need to change the information provided just submit a new form!
+                    <Text textAlign='center' alignSelf='center' size='large' weight='bold'>
+                      Please click the button below to RSVP. This button will take you to a Google Form to complete your response.
+                      If you want to continue to browse the website while you fill out the form, you can always right click on the button and then click &quot;Open Link In New Tab&quot;.
+                      After you submit the form you will recieve an email with your RSVP responses and if you ever need to change the information provided, just submit a new form!
                     </Text>
                     <Link href='https://forms.gle/8WA4Bj45UXEsvbNYA'  passHref={true}>
                         <Button primary color='black' label='Click here to RSVP'/>
