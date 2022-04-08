4c93d3c63cd9106c18f585d8b6b7f3dfe34668d7
ryanspeciale <difficultoctet@gmail.com>
2022-04-08 16:31:00 -0400

    g

--- pages/rsvp.tsx
+++ pages/rsvp.tsx
@@ -2,8 +2,8 @@ import styles from '../styles/rsvp.module.css';
 import React from 'react';
 import { Box } from 'grommet';
 import { Button } from 'grommet';
-import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { Text } from 'grommet';
 
 
 
@@ -13,9 +13,16 @@ const RSVP = () => {
     return (
         <div className={styles.container}>
             <Box>
-                <Link href='https://forms.gle/8WA4Bj45UXEsvbNYA'  passHref={true}>
-                    <Button  label='Click here to RSVP'/>
-                </Link>
+                <Box justify='center' align='center' round='medium' alignContent='center' gap='medium' margin='medium' className={styles.box}>
+                    <Text textAlign='center' alignSelf='center'>
+                     Due to this site having to be up for 6 months, and the semi-sensitive nature of the information we need from you, we have decided to hand the reins over to Google.
+                     The button below will take you to a Google Form where you will RSVP at.
+                     Click the button below and fill out the form to RSVP.
+                    </Text>
+                    <Link href='https://forms.gle/8WA4Bj45UXEsvbNYA'  passHref={true}>
+                        <Button primary color='black' label='Click here to RSVP'/>
+                    </Link>
+                </Box>
             </Box>
         </div>
     )
