72011a00f64db1c0e6c0b39ca50b5a038164dd7a
ryanspeciale <difficultoctet@gmail.com>
2022-04-08 05:11:44 -0400

    g

--- pages/rsvp.tsx
+++ pages/rsvp.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Box } from 'grommet';
 import { Button } from 'grommet';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 
 
@@ -12,7 +13,9 @@ const RSVP = () => {
     return (
         <div className={styles.container}>
             <Box>
-                
+                <Link href='https://forms.gle/8WA4Bj45UXEsvbNYA'  passHref={true}>
+                    <Button  label='Click here to RSVP'/>
+                </Link>
             </Box>
         </div>
     )
