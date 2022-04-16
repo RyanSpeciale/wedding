d225c71fd4c26e6246a8d5db55b0f434dbd49df6
ryanspeciale <difficultoctet@gmail.com>
2022-04-16 06:06:40 -0400

    made font changes to the landing page

--- components/Details.tsx
+++ components/Details.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, CardFooter, CardHeader, Heading, Text } from "grommet";
+import { Card, CardBody, CardHeader, Heading, Text } from "grommet";
 import styles from '../styles/card.module.css';
 
 
@@ -13,9 +13,8 @@ const Details = () => {
                 <CardBody className={styles.body} align="center">
                     <Text weight='bold' margin='small'>October 29th, 2022</Text>
                     <Text weight='bold' margin='small'>Ceremony at Westmont Presbyterian Church. 601 Luzerne St. Johnstown, PA 15905. 1 p.m.</Text>
-                    <Text weight='bold' margin='xsmall'>Reception to Follow at Green Gables Restaraunt. 7712 Somerset Pk. Jennerstown, PA 15547. Cocktail Hour starting at 4 p.m.</Text>
+                    <Text weight='bold' margin='small'>Reception to Follow at Green Gables Restaraunt. 7712 Somerset Pk. Jennerstown, PA 15547. Cocktail Hour starting at 4 p.m.</Text>
                 </CardBody>
-                <CardFooter></CardFooter>
             </Card>
         </div>
     );
